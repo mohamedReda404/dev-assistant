86d4948a9d398c0181bff95dd4ea3f758ee1b185
Mohamed Reda404 <muhamedreda394@gmail.com>
2026-04-05 18:04:39 +0200

    Initial project setup

---
+++ DevAssistant/Dependency Injection.cs
@@ -0,0 +1,11 @@
+﻿namespace DevAssistant
+{
+    public static class Dependency_Injection
+    {
+        public static IServiceCollection SwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            return services;
+        }
+    }
+}
